84d186d76a545dc8e0214a8835aba1cacd7071b5
KrijnS <krijnvdsteen@live.nl>
2024-05-22 16:22:24 +0200

    Always display navbar if can't scroll in page

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
+
+var body = document.body,
+html = document.documentElement;
+
+var documentHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+                   html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+if (documentHeight <= window.innerHeight) {
+   document.getElementById("navbar").style.top = "0";
+} else {
+  window.onscroll = function() {
+    scrollFunction()
+  };
+}
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
